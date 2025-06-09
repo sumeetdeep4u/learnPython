8ce9576f9120454d4312ed4ce91bec9ef3e6c6f8
Sumit Kumar <sumeetdeep4u@gmail.com>
2025-06-09 17:35:39 +0530

    Update myapp.py

--- learnPythonBasics/myapp.py
+++ learnPythonBasics/myapp.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-items = []
 
 app = FastAPI()
 
@@ -8,38 +7,41 @@ class Item(BaseModel):
     text: str = None
     is_done: bool = False
 
+# Default items
+items = [
+    Item(text="Buy milk", is_done=False),
+    Item(text="Walk the dog", is_done=True),
+    Item(text="Read a book", is_done=False)
+]
+
 @app.get("/")
 def root():
-    return {"Hello !":"World!!!!!"}
+    return {"Learn Python FastAPI Opalina!!"}
 
-@app.get("/itemlist")
+@app.get("/item-list")
 def list_item():
     return items
 
-@app.post("/items")
-def create_item(item : str):
-    items.append(item)
-    return items
-
-@app.post("/itemsm")
+@app.post("/add-item")
 def create_item(item : Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/item-list-limit")
 def create_item(limit : int = 10):
     return items[0:limit]
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
-    if item_id < len(items):
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
-
-@app.get("/itemsm/{item_id}")
+@app.get("/view-items/{item_id}")
 def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+@app.delete("/remove-items/{index}")
+def remove_item(index: int):
+    if 0 <= index < len(items):
+        removed = items.pop(index)
+        return {"message": "Item removed", "item": removed}
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
